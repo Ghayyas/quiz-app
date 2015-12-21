0c230eee44f4fedf60b350387bdd741d9d486813
Ghayyas Mubashir <Ghayyas@users.noreply.github.com>
2015-12-21 20:10:08 +0500

    Server Edit
    I edit Express-Server and Secret in cookieParser

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,53 @@
 /// <reference path="../typings/tsd.d.ts" />
 
-//third party module import
-import * as express      from "express";
-import * as morgan       from "morgan";
-import * as cookieParser from "cookie-parser";
-import * as bodyParser   from "body-parser";
-import * as path         from "path";
-import * as mongoose     from "mongoose";
+import * as express from "express";
+import * as morgan from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from  "body-parser";
+import * as path from 'path';
+import * as mongoose from 'mongoose';
+import session = require('express-session');
 
-import flash = require("connect-flash");
+import flash = require('connect-flash');
 
 //custom module import
-let index : express.Router  = require('./routes/indexRoute');
+
+let index : express.Router = require('./routes/indexRoute');
 
 //server configuration
-let app   : express.Express = express();
-let port  : number          = process.env.PORT || 3000;
-mongoose.connect('mongodb://localhost/quiz-app');
 
-// view engine setup
+let app: express.Express = express();
+let port : number = process.env.PORT || 3000;
+mongoose.connect('mongodb://localhost/quizapp');
+
+//view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 //builtin middleware
-app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.static(path.join(__dirname,'../public')));
 
 //third party middleware
+
 app.use(morgan('dev'));
-app.use(cookieParser());
+app.use(cookieParser('thisIsMySecret'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(flash());
 
 //custom mounted middleware for routing
-app.use('/', index);
 
-//start server
-app.listen(port, () => {
-  console.log('Express started on Port : ' + port);
+app.use('/',index);
+/*
+app.use('/quiz',(req,res)=>{
+	let quiz = path.join(__dirname, 'views/quiz.jade')
+	res.sendfile(quiz);
 });
+*/
+app.listen(port,()=>{
+	console.log('Express is working on port' +port);
+});
+
+
